97e5fa25eb7736d869d6004a5819a70ef25faa07
kasin <kasin.thappawan@gmail.com>
2024-11-04 16:43:48 +0700

    up 2

---
+++ BeActive/WaterView.swift
@@ -0,0 +1,20 @@
+//
+//  WaterView.swift
+//  BeActive
+//
+//  Created by Kasin Thappawan on 4/11/2567 BE.
+//
+
+import SwiftUI
+
+struct WaterView: View {
+    var body: some View {
+        Text("Water Reminder")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    WaterView()
+}

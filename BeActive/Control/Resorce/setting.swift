06392065f996976e930e03316b1d763413f95089
kasin <kasin.thappawan@gmail.com>
2025-01-25 17:12:32 +0700

    setting

---
+++ BeActive/Control/Resorce/setting.swift
@@ -0,0 +1,18 @@
+//
+//  setting.swift
+//  BeActive
+//
+//  Created by Kasin Thappawan on 25/1/2568 BE.
+//
+
+import SwiftUI
+
+struct setting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    setting()
+}
